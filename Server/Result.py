29972dc26fea8c89a371559009b931f2bc49b05f
Anh Thu <anhthu@github.com>
2023-07-18 17:56:04 +0700

    new

--- Server/Result.py
+++ Server/Result.py
@@ -3,9 +3,11 @@ import requests
 from PIL import Image
 import io
 import os
+import geocoder 
+import googlemaps
 
 # Đường dẫn đến file ảnh cần gửi lên server
-image_path = 'E:/Scientific-research/Server/images/20230519-212816-OIP.jpg'
+image_path = 'E:/demo/images/20230519-212816-OIP.jpg'
 
 # Lấy tên tệp tin ảnh từ đường dẫn
 filename = os.path.basename(image_path)
@@ -13,15 +15,30 @@ filename = os.path.basename(image_path)
 # Địa chỉ của server
 server_url = 'http://127.0.0.1:8080/upload'
 
+# Tọa độ của máy chủ gửi lên
+g = geocoder.ip('me')
+latitude, longitude = g.latlng
+
 # Đọc ảnh và chuyển đổi sang định dạng bytes
 with open(image_path, 'rb') as f:
     image_bytes = io.BytesIO(f.read())
 
 # Chuẩn bị dữ liệu để gửi yêu cầu POST đến server
 files = {'image': (filename, image_bytes, 'image/jpeg')}
+data = {'latitude': latitude, 'longitude': longitude}
 
 # Gửi yêu cầu POST đến server
-response = requests.post(server_url, files=files)
+response = requests.post(server_url, files=files, data=data)
 
-# In kết quả trả về từ server
-print(response.text)
+# Chuyển đổi giá trị latitude sang độ và phút
+lat_degree = int(abs(latitude))
+lat_minute = (abs(latitude) - lat_degree) * 60
+lat_str = "{}°{:.6f}'".format(lat_degree, lat_minute) + "N" if latitude >= 0 else "{}° {:.6f}'".format(lat_degree, lat_minute) + "S"
+
+# Chuyển đổi giá trị longitude sang độ và phút
+long_degree = int(abs(longitude))
+long_minute = (abs(longitude) - long_degree) * 60
+long_str = "{}°{:.6f}'".format(long_degree, long_minute) + "E" if longitude >= 0 else "{}° {:.6f}'".format(long_degree, long_minute) + "W"
+
+# In kết quả trả về từ server và tọa độ với kí hiệu đông/tây, nam/bắc
+print(response.text, lat_str, long_str)
