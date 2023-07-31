e135dc532dad621665e4986ecd042237e79295a8
Anh Thu <anhthu@github.com>
2023-07-31 21:00:20 +0700

    new web

--- Server/Build_web.py
+++ Server/Build_web.py
@@ -58,6 +58,14 @@ def upscale_image(image_path):
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
