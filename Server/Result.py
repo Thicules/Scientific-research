4188a186338be35d0622f9a01fde64bc30b78ada
ThaiNhatThu <21522648@gm.uit.edu.vn>
2023-09-04 16:44:05 +0700

    update profile

--- Server/Result.py
+++ Server/Result.py
@@ -6,7 +6,7 @@ import os
 import random
 
 # Đường dẫn đến file ảnh cần gửi lên server
-image_path = 'C:/Users/M S I/Downloads/download.jpg'
+image_path = 'C:/Users/Nhat Thu/Downloads/gagau.jpg'
 
 # Lấy tên tệp tin ảnh từ đường dẫn
 filename = os.path.basename(image_path)
