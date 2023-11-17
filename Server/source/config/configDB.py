27353ec1f5c0196b0aaee876095833c64efa2572
chiecbungbaton <21522371@gm.uit.edu.vn>
2023-11-17 09:24:04 +0700

    test

--- Server/source/config/configDB.py
+++ Server/source/config/configDB.py
@@ -5,7 +5,7 @@ class DB:
     DBNAME = "weblogin"
     DBIP = "localhost"
     DBUSER = "root"
-    DBPASS = "123456"
+    DBPASS = "baongan"
     def __init__(self):
         self.conn = MySQLdb.connect(self.DBIP,self.DBUSER,self.DBPASS,self.DBNAME)
 
